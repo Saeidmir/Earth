51048d6dc1dff6eea2a85a5e47bbc6571041da8e
Saeid mirakhorlo <saeid.mirakhorlo@gmail.com>
2023-04-14 16:38:50 +0430

    adding endpoint framework registration.

---
+++ Earth.Endpoints.WebApi/Extentions/DependencyInjection/AddProjectServicesExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Earth.Utilities;
+using Zamin.Extensions.Logger.Abstractions;
+
+namespace Earth.Endpoints.WebApi.Extentions.DependencyInjection;
+
+public static class AddProjectServicesExtentions
+{
+    public static IServiceCollection AddProjectUntilityServices(
+        this IServiceCollection services)
+    {
+
+        services.AddScoped<IScopeInformation, ScopeInformation>();
+        services.AddTransient<EarthAttachedServices>();
+        return services;
+    }
+
+}
+
